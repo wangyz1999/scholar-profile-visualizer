b8b1eb142c672b78d85795ac3e5d7c56225ffe2f
Yunzhe Wang <wangyz1999@hotmail.com>
2022-12-11 17:59:03 -0500

    update

--- scholar-profile.py
+++ scholar-profile.py
@@ -5,16 +5,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import json
 
 
 base_url = 'https://scholar.google.com'
 
 # Use headless mode, so you don't have to see the browser
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome('./chromedriver', options=chrome_options)
 
-def get_profile_html(name, sort_by_time=False):
+def get_profile_html(name, sort_by_time=False, more=False):
     """
     Given the name of the scholar, do a name search on Google Scholar
     Return the profile page html of the first scholar (most cited)
@@ -31,9 +33,10 @@ def get_profile_html(name, sort_by_time=False):
         profile_url += "&sortby=pubdate"
     driver.get(profile_url)
     # print(driver.page_source)
-    time.sleep(10)
-    driver.find_element('id', 'gsc_bpf_more').click()
-    time.sleep(0.3)
+    # time.sleep(10)
+    if more:
+        driver.find_element('id', 'gsc_bpf_more').click()
+        time.sleep(0.3)
     profile_html = driver.page_source
     return profile_html
     
@@ -46,10 +49,11 @@ def get_work_descriptions(profile_html):
     work_descs = []
     profile_soup = BeautifulSoup(profile_html, 'html.parser')
     pubs = profile_soup.find_all("tr", attrs={'class': 'gsc_a_tr'})
-    for p in pubs[:10]:
+    for p in tqdm(pubs):
         work_href = p.find('a')['href']
         work_url = base_url + work_href
         work_response = requests.get(work_url)
+        time.sleep(0.5)
         work_soup = BeautifulSoup(work_response.text, 'html.parser')
         try:
             desc = work_soup.find(attrs={'id': 'gsc_oci_descr'}).text
@@ -62,13 +66,19 @@ def get_interests(profile_html):
     """
     Get the scholar's research direction
     """
-    pass
+    profile_soup = BeautifulSoup(profile_html, 'html.parser')
+    pubs = profile_soup.find_all("a", attrs={'class': 'gsc_prf_inta gs_ibl'})
+    return [p.text for p in pubs]
 
 def get_index(profile_html):
     """
     Get the scholar's citations and h-index
     """
-    pass
+    profile_soup = BeautifulSoup(profile_html, 'html.parser')
+    index = profile_soup.find_all("td", attrs={'class': 'gsc_rsb_std'})
+    indexs = [i.text for i in index]
+    return indexs[0], indexs[2]
+
 
 def plot_wordcloud(text, name):
     fig = plt.figure(figsize=(10, 10))
@@ -78,13 +88,33 @@ def plot_wordcloud(text, name):
     plt.savefig(f"{name.lower().replace(' ', '_')}.png")
 
 def parse_profile(name):
-    profile_html = get_profile_html(name)
-    # interests = get_interests(profile_html)
-    # citations, hindex = get_index(profile_html)
-    work_descriptions = get_work_descriptions(profile_html)
-    all_texts = ' '.join(work_descriptions)
-    plot_wordcloud(all_texts, name)
+    profile_html = get_profile_html(name, more=False)
+    interests = get_interests(profile_html)
+    citations, hindex = get_index(profile_html)
+    # work_descriptions = get_work_descriptions(profile_html)
+    # all_texts = ' '.join(work_descriptions)
+    # plot_wordcloud(all_texts, name)
+    desc = {
+        "citations": citations,
+        "h-index": hindex,
+        "interests": interests
+    }
+    return desc
 
 if __name__ == "__main__":
-    name = 'Geoffrey Hinton'
-    parse_profile(name)
+    # name = 'Geoffrey Hinton'
+    # parse_profile(name)
+
+    school = 'harvard'
+
+    descs = dict()
+    names = open(f'{school}.txt').read().strip().split('\n')
+    for name in tqdm(names):
+        try:
+            cap_name = name.capitalize()
+            desc = parse_profile(cap_name)
+            descs[cap_name] = desc
+        except:
+            pass
+    with open(f"{school}.json", "w") as outfile:
+        json.dump(descs, outfile, indent=4)
